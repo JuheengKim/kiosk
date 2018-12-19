b161ec0be793b5e3af112d7a0f0bcccdb34574e7
dyyangs <dayeah94@gmail.com>
2018-12-19 21:19:56 +0900

    changed

--- kiosk/src/first/CheckInController.java
+++ kiosk/src/first/CheckInController.java
@@ -53,6 +53,8 @@ public class CheckInController {
 	private Label completeAdult;
 	@FXML
 	private Button checkin;
+	
+	private boolean phoneNum_switch = false;
 		
 	public CheckInController() {
 		Font.loadFont(CheckInController.class.getResource("/first/bm-Regular.ttf").toExternalForm(), 10);
@@ -105,43 +107,7 @@ public class CheckInController {
 		stage.setScene(new Scene(selectNumber));
 		stage.show();
 	}
-	
-	private void initVaribles(String num) {
-		peopleNum_ = num; 
-	    peopleNum.setText(peopleNum_);
-	}
-	
-	@FXML
-	public void adult(ActionEvent e) throws IOException, SQLException {
-		SoundEffectUtil.sound();
-		//sound
-		Media sound = new Media(new File("resource/sound/adult.mp3").toURI().toString());
-		MediaPlayer mediaPlayer = new MediaPlayer(sound);
-		mediaPlayer.play();
-		
-		Stage primaryStage = (Stage)NumberTF.getScene().getWindow(); // 현재 윈도우 가져오기
-		Parent root = null;
-	  
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("adult.fxml"));
-		root = loader.load();
-	
-		//결제 controller에 데이터 보내기
-		CheckInController controller = loader.< CheckInController>getController();
-		controller.initVaribles(NumberTF.getText());
-   
-		Scene scene = new Scene(root);    
-		primaryStage.setScene(scene);
-		primaryStage.show();	
-	}
-	
-	int j = 0;
-	public void adultIncrement() {
-		SoundEffectUtil.sound();
-		
-		j++;
-		String ttt = Integer.toString(j);
-		completeAdult.setText(ttt);
-	}
+
 	
 	@FXML
 	public void backCheckIn(ActionEvent e) throws IOException {
@@ -179,17 +145,99 @@ public class CheckInController {
 		NumberTF.setText(tmp);
 	}
 	
-	public void privateInfo(ActionEvent e) throws IOException {
-		SoundEffectUtil.sound();	
-		//sound
-		Media sound = new Media(new File("resource/sound/phone.mp3").toURI().toString());
-		MediaPlayer mediaPlayer = new MediaPlayer(sound);
-		mediaPlayer.play();
+	
+	private void initVaribles(String num) {
+		peopleNum_ = num; 
+	    peopleNum.setText(peopleNum_);
+	}
+	
+	@FXML
+	public void adult(ActionEvent e) throws IOException, SQLException {
 		
-		Parent selectNumber = FXMLLoader.load(getClass().getResource("privateInfo.fxml"));
-		Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-		stage.setScene(new Scene(selectNumber));
-		stage.show();
+		if(Integer.parseInt(NumberTF.getText())<1)
+		{
+	          String alertStr = "숙박인원은 1명이상만 가능합니다!";
+	          Alert alert = new Alert(AlertType.ERROR);
+	          alert.setContentText(alertStr);
+	          alert.showAndWait();
+		}
+		else
+		{
+			
+			SoundEffectUtil.sound();
+			//sound
+			Media sound = new Media(new File("resource/sound/adult.mp3").toURI().toString());
+			MediaPlayer mediaPlayer = new MediaPlayer(sound);
+			mediaPlayer.play();
+			
+			Stage primaryStage = (Stage)NumberTF.getScene().getWindow(); // 현재 윈도우 가져오기
+			Parent root = null;
+		  
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("adult.fxml"));
+			root = loader.load();
+		
+			//결제 controller에 데이터 보내기
+			CheckInController controller = loader.< CheckInController>getController();
+			controller.initVaribles(NumberTF.getText());
+	   
+			Scene scene = new Scene(root);    
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		}
+	}
+	
+	int j = 0;
+	public void adultIncrement() {
+		SoundEffectUtil.sound();
+		if(j < Integer.parseInt(peopleNum.getText()))
+		{
+			j++;
+		}
+		else if(j==Integer.parseInt(peopleNum.getText()) )
+		{
+	          String alertStr = "다음 화면으로 넘어가세요!";
+	          Alert alert = new Alert(AlertType.ERROR);
+	          alert.setContentText(alertStr);
+	          alert.showAndWait();
+		}
+		else
+		{
+			;
+		}
+			
+			
+		String ttt = Integer.toString(j);
+		completeAdult.setText(ttt);
+	}
+	
+	
+	public void privateInfo(ActionEvent e) throws IOException {
+		
+		if(Integer.parseInt(completeAdult.getText())!=Integer.parseInt(peopleNum.getText()))
+		{
+	          String alertStr = "성인인증이 완료되지 않았습니다!";
+	          Alert alert = new Alert(AlertType.ERROR);
+	          alert.setContentText(alertStr);
+	          alert.showAndWait();
+			
+		}
+		else
+		{
+			SoundEffectUtil.sound();	
+			//sound
+			Media sound = new Media(new File("resource/sound/phone.mp3").toURI().toString());
+			MediaPlayer mediaPlayer = new MediaPlayer(sound);
+			mediaPlayer.play();
+			
+			Parent selectNumber = FXMLLoader.load(getClass().getResource("privateInfo.fxml"));
+			Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+			stage.setScene(new Scene(selectNumber));
+			stage.show();
+	
+		}
+	
+		
 	}
 
 	// phone
@@ -230,62 +278,80 @@ public class CheckInController {
         if(!Pattern.matches("(010|011|016|017|018?019)(.+)(.{4})", phoneNum.getText()))
         {
             String alertStr = "유효하지 않은 번호입니다. 다시 입력하세요.";
-            Alert alert = new Alert(AlertType.INFORMATION);
+            Alert alert = new Alert(AlertType.ERROR);
             alert.setHeaderText(phoneNum.getText());
             alert.setContentText(alertStr);
             alert.showAndWait();
             phoneNum.setText("");
             return;
         }
+        
+        phoneNum_switch = true;
 	 }
 	
 	/** 확인 버튼 누를 때 
 	 * @throws SQLException 
 	 * @throws IOException **/
 	public void roomList(ActionEvent event) throws SQLException, IOException {
-		SoundEffectUtil.sound();
-		Stage primaryStage = (Stage)name.getScene().getWindow(); // 현재 윈도우 가져오기
-		Parent root = null;
 		
-	    //공백 체크
-	    if(phoneNum.getText().length() == 0)
-	    {
-	          String alertStr = "연락처를 입력해주세요!";
-	          Alert alert = new Alert(AlertType.INFORMATION);
-	          alert.setHeaderText(phoneNum.getText());
-	          alert.setContentText(alertStr);
-	          alert.showAndWait();
-	    } 
-	    else {
-	        
-	        connection = ConnectionUtil.connectdb();
-	        String sql = "INSERT INTO User (uname, utel, jumin) VALUES(?,?,?)";
-	        
-	        preparedStatement = connection.prepareStatement(sql);
-	
-	        preparedStatement.setString(1, name.getText());
-	        preparedStatement.setString(3, jumin.getText());
-	        preparedStatement.setString(2, phoneNum.getText());
-	   
- 	       	int uno = preparedStatement.executeUpdate();
- 	       	
- 	       	//sound
- 			Media sound = new Media(new File("resource/sound/selroom.mp3").toURI().toString());
- 			MediaPlayer mediaPlayer = new MediaPlayer(sound);
- 			mediaPlayer.play();
-	 		
-	 		 FXMLLoader loader = new FXMLLoader(getClass().getResource("/second/RoomList.fxml"));
-	 		 root = loader.load();
-	 		 
-	 		 user = new User(uno, jumin.getText(), name.getText(), phoneNum.getText());
-	
-	 		 //결제 controller에 데이터 보내기
-	 		 RoomListController controller = loader.<RoomListController>getController();
-	 		 controller.initVariable(user, null, null, null);
-	 		
-	 	     Scene scene = new Scene(root);    
-	 	     primaryStage.setScene(scene);
-	 	     primaryStage.show();
-	    }
+		if(phoneNum_switch == false)
+		{
+            String alertStr = "휴대폰 번호를 입력하세요!";
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setContentText(alertStr);
+            alert.showAndWait();
+			
+		}
+		else
+		{
+			
+			SoundEffectUtil.sound();
+			Stage primaryStage = (Stage)name.getScene().getWindow(); // 현재 윈도우 가져오기
+			Parent root = null;
+			
+		    //공백 체크
+		    if(phoneNum.getText().length() == 0)
+		    {
+		          String alertStr = "연락처를 입력해주세요!";
+		          Alert alert = new Alert(AlertType.INFORMATION);
+		          alert.setHeaderText(phoneNum.getText());
+		          alert.setContentText(alertStr);
+		          alert.showAndWait();
+		    } 
+		    else 
+		    {
+		        
+		        connection = ConnectionUtil.connectdb();
+		        String sql = "INSERT INTO User (uname, utel, jumin) VALUES(?,?,?)";
+		        
+		        preparedStatement = connection.prepareStatement(sql);
+		
+		        preparedStatement.setString(1, name.getText());
+		        preparedStatement.setString(3, jumin.getText());
+		        preparedStatement.setString(2, phoneNum.getText());
+		   
+	 	       	int uno = preparedStatement.executeUpdate();
+	 	       	
+	 	       	//sound
+	 			Media sound = new Media(new File("resource/sound/selroom.mp3").toURI().toString());
+	 			MediaPlayer mediaPlayer = new MediaPlayer(sound);
+	 			mediaPlayer.play();
+		 		
+		 		 FXMLLoader loader = new FXMLLoader(getClass().getResource("/second/RoomList.fxml"));
+		 		 root = loader.load();
+		 		 
+		 		 user = new User(uno, jumin.getText(), name.getText(), phoneNum.getText());
+		
+		 		 //결제 controller에 데이터 보내기
+		 		 RoomListController controller = loader.<RoomListController>getController();
+		 		 controller.initVariable(user, null, null, null);
+		 		
+		 	     Scene scene = new Scene(root);    
+		 	     primaryStage.setScene(scene);
+		 	     primaryStage.show();
+		 	     
+		 	    
+		    }
+		}
 	} 
 }
--- kiosk/src/util/ConnectionUtil.java
+++ kiosk/src/util/ConnectionUtil.java
@@ -12,7 +12,9 @@ public class ConnectionUtil {
         {
             Class.forName("com.mysql.cj.jdbc.Driver");
             //Connection conn = DriverManager.getConnection("jdbc:mysql://ec2-18-191-143-160.us-east-2.compute.amazonaws.com:3306/hotel","root","0718");
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/kioskdb?serverTimezone=UTC&useSSL=false","root","0718");
+            //Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/kioskdb?serverTimezone=UTC&useSSL=false","root","0718");
+           // Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/kioskdb?serverTimezone=UTC&useSSL=false","root","tkfkdgo12");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://ec2-18-191-9-59.us-east-2.compute.amazonaws.com:3306/kioskDB","root","0718");
             return conn;
         }
         catch(Exception e)
